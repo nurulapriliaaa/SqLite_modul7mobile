7e514f64f7dce267ff7d835d16d53c1ef343173d
nurulapriliaaa <nurulapriliaaa@gmail.com>
2023-12-16 19:14:38 -0800

    Initial commit

---
+++ app/src/main/java/com/nurulaprilia/sqlite_modul7mobile/MainActivity.java
@@ -0,0 +1,41 @@
+package com.nurulaprilia.sqlite_modul7mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.nurulaprilia.sqlite_modul7mobile.databinding.ActivityMainBinding;
+import com.nurulaprilia.sqlite_modul7mobile.utils.DatabaseHelper;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        databaseHelper = new DatabaseHelper(this);
+        binding.btnLogin.setOnClickListener(view -> {
+            String username = binding.edtUsername.getText().toString().trim();
+            String password = binding.edtPassword.getText().toString().trim();
+            boolean res = databaseHelper.checkUser(username, password);
+
+            if (res){
+                Toast.makeText(MainActivity.this, "Succesfully Logged In", Toast.LENGTH_SHORT).show();
+                Intent contentIntent = new Intent(MainActivity.this, ContentActivity.class);
+                startActivity(contentIntent);
+            }else {
+                Toast.makeText(MainActivity.this, "Username atau Password Anda Salah", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.btnRegister.setOnClickListener(view -> {
+            Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(registerIntent);
+        });
+    }
+}
---
+++ app/src/main/java/com/nurulaprilia/sqlite_modul7mobile/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.nurulaprilia.sqlite_modul7mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.nurulaprilia.sqlite_modul7mobile.databinding.ActivityRegisterBinding;
+import com.nurulaprilia.sqlite_modul7mobile.utils.DatabaseHelper;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        databaseHelper = new DatabaseHelper(this);
+
+        binding.btnLogin.setOnClickListener(view -> {
+            Intent LoginIntent =
+                    new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(LoginIntent);
+        });
+
+        binding.btnRegister.setOnClickListener(view -> {
+            String username = binding.edtUsername.getText().toString().trim();
+            String password = binding.edtPassword.getText().toString().trim();
+            String confPass = binding.edtConfirm.getText().toString().trim();
+
+            if (password.equals(confPass)){
+                long pal = databaseHelper.addUser(username,password);
+
+                if (pal > 0){
+                    Toast.makeText(RegisterActivity.this, "You have registered.",
+                            Toast.LENGTH_SHORT).show();
+                    Intent moveToLogin =
+                            new Intent(RegisterActivity.this,MainActivity.class);
+                    startActivity(moveToLogin);
+                }
+            } else {
+                Toast.makeText(RegisterActivity.this,"Password is not matching.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
